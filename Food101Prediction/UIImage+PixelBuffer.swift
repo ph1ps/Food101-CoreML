d87fda487536914b054a3f0d99ef43113f38122a
ph1ps <ph1psdev@gmail.com>
2018-02-15 18:05:24 +0100

    Move extension to seperate file, swiftlint, remove generated comments

---
+++ Food101Prediction/UIImage+PixelBuffer.swift
@@ -0,0 +1,54 @@
+//
+//  UIImage+PixelBuffer.swift
+//  Food101Prediction
+//
+//  Created by Philipp Gabriel on 15.02.18.
+//  Copyright © 2018 Philipp Gabriel. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+
+    func resize(to newSize: CGSize) -> UIImage? {
+
+        guard self.size != newSize else { return self }
+
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        self.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+
+        defer { UIGraphicsEndImageContext() }
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+
+    func pixelBuffer() -> CVPixelBuffer? {
+
+        let width = Int(self.size.width)
+        let height = Int(self.size.height)
+
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer: CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, width, height, kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard status == kCVReturnSuccess else {
+            return nil
+        }
+
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        guard let context = CGContext(data: pixelData, width: width, height: height, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {
+            return nil
+        }
+
+        context.translateBy(x: 0, y: CGFloat(height))
+        context.scaleBy(x: 1.0, y: -1.0)
+
+        UIGraphicsPushContext(context)
+        self.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+
+        return pixelBuffer
+    }
+}
--- Food101Prediction/ViewController.swift
+++ Food101Prediction/ViewController.swift
@@ -9,105 +9,63 @@
 import UIKit
 import CoreML
 
-extension UIImage {
-
-    func resize(to newSize: CGSize) -> UIImage? {
-
-        guard self.size != newSize else { return self }
-        
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
-        self.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
-        
-        defer { UIGraphicsEndImageContext() }
-        return UIGraphicsGetImageFromCurrentImageContext()
-    }
-    
-    func pixelBuffer() -> CVPixelBuffer? {
-        
-        let width = Int(self.size.width)
-        let height = Int(self.size.height)
-        
-        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
-        var pixelBuffer: CVPixelBuffer?
-        let status = CVPixelBufferCreate(kCFAllocatorDefault, width, height, kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
-        guard status == kCVReturnSuccess else {
-            return nil
-        }
-        
-        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
-        
-        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-        let context = CGContext(data: pixelData, width: width, height: height, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
-        
-        context?.translateBy(x: 0, y: CGFloat(height))
-        context?.scaleBy(x: 1.0, y: -1.0)
-        
-        UIGraphicsPushContext(context!)
-        self.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
-        UIGraphicsPopContext()
-        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-        
-        return pixelBuffer
-    }
-}
-
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
+
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var percentage: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        percentage.text = ""
-//        imageView.isUserInteractionEnabled = true
-//        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapImage))
-//        imageView.addGestureRecognizer(gestureRecognizer)
-        
     }
-    
+
     @IBAction func buttonPressed(_ sender: Any) {
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         let imagePickerView = UIImagePickerController()
         imagePickerView.delegate = self
-        alert.addAction(UIAlertAction(title: "Choose Image", style: .default, handler: { (alert) in
+
+        alert.addAction(UIAlertAction(title: "Choose Image", style: .default) { _ in
             imagePickerView.sourceType = .photoLibrary
             self.present(imagePickerView, animated: true, completion: nil)
-        }))
-        alert.addAction(UIAlertAction(title: "Take Image", style: .default, handler: { (alert) in
+        })
+
+        alert.addAction(UIAlertAction(title: "Take Image", style: .default) { _ in
             imagePickerView.sourceType = .camera
             self.present(imagePickerView, animated: true, completion: nil)
-        }))
+        })
+
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
-    
+
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        let image = info["UIImagePickerControllerOriginalImage"] as! UIImage
-        processImage(image)
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
         dismiss(animated: true, completion: nil)
+
+        guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else {
+            return
+        }
+
+        processImage(image)
     }
-    
+
     func processImage(_ image: UIImage) {
         let model = Food101()
         let size = CGSize(width: 299, height: 299)
-        
+
         guard let buffer = image.resize(to: size)?.pixelBuffer() else {
             fatalError("Scaling or converting to pixel buffer failed!")
         }
-        
+
         guard let result = try? model.prediction(image: buffer) else {
             fatalError("Prediction failed!")
         }
-        
+
         let confidence = result.foodConfidence["\(result.classLabel)"]! * 100.0
         let converted = String(format: "%.2f", confidence)
-        
+
         imageView.image = image
         percentage.text = "\(result.classLabel) - \(converted) %"
     }
